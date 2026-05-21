8f872808bdc656c1cbc05e76af0e5f7bca910491
Naveen Sharma <naveensharmaaurangabad@gmail.com>
2026-05-22 00:45:02 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,61 @@
+
+#comparison operators
+a = 7
+b = 2
+print(a//b)
+
+a = 10
+b = 3
+print(a//b)
+
+
+a = 7
+b = 5
+print(a != b)
+
+a = 5
+b = 5
+print(a == b)
+
+a = 5
+b = 2
+print(a == b)
+
+num1 = 95
+num2 = 169
+res = num1>num2
+print(res)
+
+num3 = False
+print(res == num3)
+
+num3 = True
+print(res != num3)
+
+
+#Logical operators
+a = 5
+b = 50
+print(a >= b and a==b)
+
+a = 50
+b = 50
+print(a >= b and a==b)
+
+a = 5
+b = 50
+print(a <= b or a==b)
+
+print(not True)
+print(not False)
+
+
+#Assignment Operators
+x = 5
+x = x+3
+print(x)
+
+x = 5
+x += 3
+print(x)
+
